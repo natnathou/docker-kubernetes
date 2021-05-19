a231def6a4c8a0eddd753f5ef4bbfb1029f7267f
natnathou <ne.zaffran@gmail.com>
2021-05-20 00:07:08 +0300

    commande watch and automatic rebuild docker

--- node-server-base/NodeProject/index.js
+++ node-server-base/NodeProject/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('It`s magical');
+  res.send('It`s magical????');
 });
 
 app.listen(8080, () => {
